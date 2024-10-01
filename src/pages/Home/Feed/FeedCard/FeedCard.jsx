a99edabc348da5f42aa15239ee641170dd82806c
Gustavo Portela <ggomesportela26@gmail.com>
2024-09-30 22:27:47 -0300

    update like.jsx

--- src/pages/Home/Feed/FeedCard/FeedCard.jsx
+++ src/pages/Home/Feed/FeedCard/FeedCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import InstagramFeed from '../FeedData/FeedData';
 import Ellipse from '../../../../components/Icons/Ellipse/Ellipse.jsx';
-import Like from '../../../../components/Icons/Like/like.jsx';
+import Like from '../../../../components/Icons/Like/Like.jsx';
 import Comment from '../../../../components/Icons/Comment/Comment.jsx';
 import Share from '../../../../components/Icons/Share/Share.jsx';
 import Save from '../../../../components/Icons/Save/Save.jsx';

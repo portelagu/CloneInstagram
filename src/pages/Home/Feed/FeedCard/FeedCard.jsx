d6b9271b43a744252a844727fb3c8425a2e7f7ed
Gustavo Portela <ggomesportela26@gmail.com>
2024-09-30 22:24:05 -0300

    update type of file

--- src/pages/Home/Feed/FeedCard/FeedCard.jsx
+++ src/pages/Home/Feed/FeedCard/FeedCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import InstagramFeed from '../FeedData/FeedData';
-import Ellipse from '../../../../components/Icons/Ellipse/Ellipse';
-import Like from '../../../../components/Icons/Like/like';
-import Comment from '../../../../components/Icons/Comment/Comment';
-import Share from '../../../../components/Icons/Share/Share';
-import Save from '../../../../components/Icons/Save/Save';
-import Emoji from '../../../../components/Icons/Emoji/Emoji';
+import Ellipse from '../../../../components/Icons/Ellipse/Ellipse.jsx';
+import Like from '../../../../components/Icons/Like/like.jsx';
+import Comment from '../../../../components/Icons/Comment/Comment.jsx';
+import Share from '../../../../components/Icons/Share/Share.jsx';
+import Save from '../../../../components/Icons/Save/Save.jsx';
+import Emoji from '../../../../components/Icons/Emoji/Emoji.jsx';
 
 
 const FeedCard = () => {
